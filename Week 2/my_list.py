348e8108eb22097b8fa620fdaf2d23ffd3604186
ColeOnTop <collin.sowino2847@gmail.com>
2025-08-06 18:31:46 +0300

    week 2 Assignment

---
+++ Week 2/my_list.py
@@ -0,0 +1,25 @@
+
+#Create list
+my_list = []
+
+#Appended Numbers
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#Value Insertion
+my_list.insert(1,15)
+
+#List Extension
+second_list = [50,60,70]
+
+my_list.extend(second_list)
+ 
+#Removing
+my_list.remove(70)
+
+#Sorting
+my_list.sort()
+print(my_list[3])
+
